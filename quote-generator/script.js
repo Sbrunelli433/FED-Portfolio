e369f6b25c392b81e8de379cac14b7bba7c41e55
Steve Brunelli <brunes46@gmail.com>
2021-05-19 13:08:50 -0500

    added loader spinning feature and functionality

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,8 +3,19 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // //created backup newQuote function using quotes.js
 // function newQuote() {
@@ -14,6 +25,7 @@ let apiQuotes = [];
 
 //new quote
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     //check if author field is blank and replace it with 'unknown'
@@ -29,9 +41,11 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
