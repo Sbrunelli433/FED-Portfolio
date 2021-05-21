f65df938c78cb032cf5e8459ea330e5b1edb9e1f
Steve Brunelli <brunes46@gmail.com>
2021-05-21 11:30:19 -0500

    added javascript functionality to the media control buttons. prev and next song buttons loop through the songs array.

--- music-player-feature/script.js
+++ music-player-feature/script.js
@@ -1,9 +1,36 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
 let isPlaying = false;
+
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+      },
+      {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+      },
+      {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design',
+      },
+];
 
 function playSong() {
     isPlaying = true;
@@ -20,3 +47,38 @@ function pauseSong() {
 }
 
 playBtn.addEventListener('click',  () => (isPlaying ? pauseSong() : playSong()));
+
+//update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+function nextSong() {
+    songIndex++;
+    if(songIndex > songs.length -1) {
+        songIndex = 0;
+    }
+    console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+function prevSong() {
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+
+let songIndex = 0;
+loadSong(songs[songIndex]);
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
