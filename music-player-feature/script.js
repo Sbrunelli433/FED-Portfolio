182fa5adadb597cf9d717fb4b955fdfeb72de5a5
Steve Brunelli <brunes46@gmail.com>
2021-05-21 11:13:49 -0500

    added play/pause button functionality to alternate depending on the status of the button.

--- music-player-feature/script.js
+++ music-player-feature/script.js
@@ -7,12 +7,15 @@ let isPlaying = false;
 
 function playSong() {
     isPlaying = true;
-
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
 function pauseSong() {
     isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
